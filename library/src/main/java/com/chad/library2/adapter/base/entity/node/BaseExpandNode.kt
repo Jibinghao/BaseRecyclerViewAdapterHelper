1c17876d380ff2b8e4fc939a76ef131c995a7470
jibinghao <769499597@qq.com>
2020-07-15 16:52:01 +0800

    修改包名

---
+++ library/src/main/java/com/chad/library2/adapter/base/entity/node/BaseExpandNode.kt
@@ -0,0 +1,6 @@
+package com.chad.library2.adapter.base.entity.node
+
+abstract class BaseExpandNode : BaseNode() {
+    var isExpanded: Boolean = true
+
+}
---
+++ library/src/main/java/com/chad/library2/adapter/base/listener/OnLoadMoreListener.java
@@ -0,0 +1,11 @@
+package com.chad.library2.adapter.base.listener;
+
+/**
+ * @author: limuyang
+ * @date: 2019-12-03
+ * @Description:
+ */
+public interface OnLoadMoreListener {
+
+    void onLoadMore();
+}
